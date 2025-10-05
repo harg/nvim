ede286ca8df72edceee3c16667c044f8413b3824
harg <harold.gay@gmail.com>
2025-10-05 21:30:41 +0200

    update conf

--- lua/gui.lua
+++ lua/gui.lua
@@ -20,7 +20,7 @@ if vim.g.neovide then
   -- vim.o.guifont = "UnifontExMono:b:h12:#h-none"
   -- vim.o.guifont = "Consolas_Nerd_Font_Mono:h12.4:#e-subpixelantialias:#h-slight"
   --
-  vim.o.linespace = 2
+  vim.o.linespace = 5
   vim.opt.guicursor = {
     "n-v-c:block-Cursor/lCursor", -- Block cursor in normal, visual, and command modes
     "i:ver25-blinkwait500-blinkoff700-blinkon700-Cursor/lCursor", -- Blinking vertical line in insert mode
@@ -54,6 +54,6 @@ if vim.g.nvy then
   -- vim.opt.guifont = { "Cascadia Code FR:h10.5" }
   -- vim.opt.guifont = { "Office Code Pro:h11.5" }
   -- vim.opt.guifont = { "NotoMono NFM:h11" }
-  vim.opt.guifont = { "Hack:h11" }
+  vim.opt.guifont = { "PragmataPro Mono:h12" }
   -- vim.opt.guifont = { "Inc0rsolata:h12.5" }
 end
